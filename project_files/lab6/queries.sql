6ee13817b8958d914fd9b731e21703644006b4cf
brettmccausland <brettmccausland@yahoo.com>
2018-11-11 22:48:44 -0800

    s

--- project_files/lab6/queries.sql
+++ project_files/lab6/queries.sql
@@ -10,7 +10,7 @@ GROUP BY C.sid;
 -- FROM Catalog C,Suppliers S
 -- GROUP BY C.sid
 -- HAVING COUNT(PartCount)>3;
-SELECT COUNT(C.pid),Suppliers.sid
+SELECT COUNT(C.pid),S.sid
 FROM Suppliers S, Catalog C
 WHERE S.sid = C.sid
 GROUP BY S.sid
