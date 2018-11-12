64836db8102ce5c7dbe77aab340322785322f1b3
brettmccausland <brettmccausland@yahoo.com>
2018-11-12 14:32:40 -0800

    r

--- project_files/lab6/EmbeddedSQL.java
+++ project_files/lab6/EmbeddedSQL.java
@@ -252,7 +252,8 @@ public class EmbeddedSQL {
      //1. Find the total number of parts supplied by each supplier");
      try{
         String query = "SELECT COUNT(catalog.pid), catalog.sid FROM catalog GROUP BY catalog.sid;";
-        esql.executeQuery(query);
+        int rowCount = esql.executeQuery(query);
+        System.out.println ("total row(s): " + rowCount);
      }catch(Exception e){
         System.err.println (e.getMessage());
      }
@@ -262,7 +263,8 @@ public class EmbeddedSQL {
      //2. Find the total number of parts supplied by each supplier who supplies at least 3 parts");
      try{
       String query = "SELECT COUNT(catalog.pid) AS PartCount, catalog.sid,FROM catalog, GROUP BY catalog.sid, HAVING COUNT(PartCount)>3;";
-      esql.executeQuery(query);
+        int rowCount =esql.executeQuery(query);
+      System.out.println ("total row(s): " + rowCount);
      }catch(Exception e){
         System.err.println (e.getMessage());
      }
@@ -272,7 +274,8 @@ public class EmbeddedSQL {
     //3. For every supplier that supplies only green parts, print the name of the supplier and the total number of parts that he supplies");
      try{
      String query = "SELECT COUNT(C.pid),S.sid,S.sname FROM Suppliers S, Catalog C WHERE S.sid = C.sid and S.sid = C.sid and S.sid NOT IN ( SELECT S2.sid FROM Suppliers S2, Parts P2, Catalog C2 WHERE S2.sid = C2.sid and P2.pid = C2.pid and P2.Color != 'Green') GROUP BY S.sid;";
-     esql.executeQuery(query);
+     int rowCount = esql.executeQuery(query);
+     System.out.println ("total row(s): " + rowCount);
     }catch(Exception e){
        System.err.println (e.getMessage());
     }
