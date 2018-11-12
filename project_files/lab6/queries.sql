0a85bda2786220ec20ae5e5de7392ededd6f273c
brettmccausland <brettmccausland@yahoo.com>
2018-11-12 13:06:35 -0800

    s

--- project_files/lab6/queries.sql
+++ project_files/lab6/queries.sql
@@ -20,8 +20,14 @@ SELECT COUNT(C.pid),S.sid,S.sname
 FROM Suppliers S, Catalog C
 WHERE S.sid = C.sid
 and S.sid = C.sid
+and S.sid NOT IN (
+          SELECT S2.sid
+          FROM Suppliers S2, Parts P2, Catalog C2
+          WHERE S2.sid = C2.sid
+          and P2.pid = C2.pid
+          and P2.Color != 'Green')
 GROUP BY S.sid
--- and S.sid
+
 -- IN (SELECT S1.sid
 --     FROM Suppliers S1, Parts P1, Catalog C1
 --     WHERE S1.sid = C1.sid
